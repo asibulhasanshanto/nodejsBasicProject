ffe889e6f964409a163f8bb50adf06aeb5fa9ebb
asibulhasanshanto <asibulhasan.rucse.18@gmail.com>
2021-12-10 16:50:37 +0600

    basic error handler and app and server file completed

--- app.js
+++ app.js
@@ -1,6 +1,64 @@
 const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
+//routers
+// const userRouter = require('./routes/userRoutes');
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price'
+        ]
+    })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
@@ -9,4 +67,10 @@ app.use((req, res, next) => {
     next();
 });
 
+//ROUTES
+// app.use('/api/v1/users',userRouter);
+
+app.use(globalErrorHandler);
+
 module.exports = app;
+
--- server.js
+++ server.js
@@ -1,5 +1,41 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//catches any uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const server = app.listen(3000, () => {
-    console.log('App is running');
+// //database config for hosted database
+// const DB = process.env.DATABASE.replace(
+//     '<PASSWORD>',
+//     process.env.DATABASE_PASSWORD
+//   );
+
+//database config for local database
+const DB = process.env.DATABASE_LOCAL;
+
+//connect to databse
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
